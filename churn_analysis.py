2b946a28e81c5199605623fba05b308026f63260
IvanMinev9 <i.minev.m@gmail.com>
2026-03-20 16:09:39 +0200

    Churn project basic

---
+++ churn_analysis.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("Churn_Modelling.csv")
+
+# Колко клиента са напуснали
+print("Churn count:")
+print(df["Exited"].value_counts())
+
+# Средна възраст
+print("\nAverage age:")
+print(df.groupby("Exited")["Age"].mean().round(2))
+
+# Баланс
+print("\nAverage balance:")
+print(df.groupby("Exited")["Balance"].mean().round(2))
+
+# Брой продукти
+print("\nProducts:")
+print(df.groupby("Exited")["NumOfProducts"].mean())
